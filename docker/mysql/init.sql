2ceb2b2bcbdf33789ddab1bf8e8954263bafc63d
kiraTheresa <2312190107@pop.zjgsu.edu.cn>
2026-01-12 23:05:32 +0800

    add

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -19,4 +19,10 @@ INSERT INTO medicines (name, dosage, hour, minute, box_num, enabled) VALUES
 ('维生素', '2粒', 18, 30, 2, TRUE),
 ('心脑通', '1粒', 9, 0, 2, TRUE),
 ('钙片', '1片', 20, 0, 1, TRUE),
-('鱼油', '1粒', 14, 30, 2, TRUE);
+('鱼油', '1粒', 14, 30, 2, TRUE),
+('Lisinopril', '10mg', 8, 0, 1, TRUE),
+('Metformin', '500mg', 12, 0, 1, TRUE),
+('Vitamin D', '2000 IU', 18, 30, 2, TRUE),
+('Aspirin', '81mg', 9, 0, 2, TRUE),
+('Calcium Carbonate', '600mg', 20, 0, 1, TRUE),
+('Omega-3', '1000mg', 14, 30, 2, TRUE);
